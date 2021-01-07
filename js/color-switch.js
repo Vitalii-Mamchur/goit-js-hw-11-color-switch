f77a08d75650af4a503550f8a9b3b78185be5308
Vitalii-Mamchur <65975103+Vitalii-Mamchur@users.noreply.github.com>
2021-01-07 16:36:12 +0200

    Updates

---
+++ js/color-switch.js
@@ -0,0 +1,47 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+}
+
+const colorСhanging = {
+  intervalId: null,
+  changingActiv: false,
+  min: 0,
+  max: colors.length - 1,
+
+  start() {
+    if (this.changingActiv) {
+      return;
+    }
+
+    this.changingActiv = true;
+    this.intervalId = setInterval(() => {
+      const randomIntegerFromInterval = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      };
+      const bgColor = colors[randomIntegerFromInterval(this.min, this.max)];
+      refs.body.style.background = bgColor;
+    }, 1000);
+    refs.startBtn.disabled = true;
+  },
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+    this.changingActiv = false;
+    refs.startBtn.disabled = false;
+  },
+};
+
+refs.startBtn.addEventListener('click', colorСhanging.start.bind(colorСhanging));
+refs.stopBtn.addEventListener('click', colorСhanging.stop.bind(colorСhanging));
